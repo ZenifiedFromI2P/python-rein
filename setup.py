87c1da2f4aa0f64b2543d6dd3348973bdac41df6
David Sterry <dsterry@uber.com>
2016-11-22 09:44:48 -0800

    possible fix for python on windows

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
+    package_data={'': ['rein/html/*']}
     install_requires=dependencies,
     entry_points={
         'console_scripts': [
@@ -25,10 +26,7 @@ setup(
         ],
     },
     classifiers=[
-        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        # 'Development Status :: 1 - Planning',
-         'Development Status :: 2 - Pre-Alpha',
-        # 'Development Status :: 3 - Alpha',
+         'Development Status :: 3 - Alpha',
         # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
@@ -42,7 +40,6 @@ setup(
         'Operating System :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
